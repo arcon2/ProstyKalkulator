7324439ebada10fdacc2a66f243f9cd45e15879a
Arcon <arek@websitebest.pl>
2017-07-14 20:23:08 +0200

    Pierwsza klasa mars applications

---
+++ src/MarsApplication.java
@@ -0,0 +1,44 @@
+/**
+ * Created by Arcon on 2017-06-24.
+ */
+public class MarsApplication
+{
+    public static void main(String[] args)
+    {
+        MarsRobot spirit = new MarsRobot();
+        MarsRobot opportunity = new MarsRobot();
+        opportunity.status = "Eksplatacja";
+        spirit.status = "Eksploracja";
+
+        opportunity.speed=3;
+        spirit.speed =2;
+
+        spirit.temperature=-60;
+        opportunity.temperature=-66;
+
+        opportunity.showAttributes();
+        spirit.showAttributes();
+
+        System.out.println("Zwiększ prędkość do 3");
+        spirit.speed=3;
+        opportunity.speed=4;
+
+        System.out.println("Zwiększono prędkość do "+spirit.speed);
+        spirit.showAttributes();
+        opportunity.showAttributes();
+
+        System.out.println("Zmiana tem na -90");
+        spirit.temperature=-90;
+        opportunity.temperature=-80;
+
+        spirit.showAttributes();
+        opportunity.showAttributes();
+
+        System.out.println("Sprawdzanie tem");
+        spirit.checkTemperature();
+        opportunity.checkTemperature();
+        spirit.showAttributes();
+        opportunity.showAttributes();
+
+    }
+}
