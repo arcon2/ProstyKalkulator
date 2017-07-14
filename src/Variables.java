3afdb4c86d02cd112ca27cb3079199dca3cbf9b5
Arcon <arek@websitebest.pl>
2017-07-14 21:09:26 +0200

    trzecia klasa variables

--- src/Variables.java
+++ src/Variables.java
@@ -5,14 +5,20 @@ public class Variables
 {
     public static void main(String[] args)
     {
-        final char UP='U';
+        final char UP='U'; //zmienna finalna
         byte initialLevel = 12;
         short location = 13250;
         int score = 3500100;
         boolean newGame = true;
+        int year = 2017;
+        int a,b,c;
+        a=20;
+        b=9;
 
         System.out.println("Poziom: "+initialLevel);
         System.out.println("W górę: "+UP);
+        System.out.println("Jest rok: "+year);
+        System.out.println(c=a%b);
 
 
 
