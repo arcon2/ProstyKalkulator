194fb62f705573f7d5e02bd0fc1cef345a232b8e
Arcon <arek@websitebest.pl>
2017-11-19 19:09:24 +0100

    Połaczenie z bazą

--- BazaDanych.java
+++ BazaDanych.java
@@ -2,62 +2,52 @@ package Program;
 
 import java.sql.*;
 
+
 public class BazaDanych {
 
-    static String daneZBazy;
 
-    /**
-     * @param args
-     */
-    //public static void main(String[] args) {
-    public void baza() {
-        String polaczenieURL = "jdbc:mysql://192.168.1.4/test?user=test&password=test";
-        //Tworzymy proste zapytanie doa bazy danych
-        String query = "Select * FROM osoba";
+    public static final String sterownik = "com.mysql.jdbc.Driver";
+    public static final String baza = "jdbc:mysql://localhost:3306/dane";
 
-        Connection conn = null;
+    Connection conn;
+    Statement stat;
 
+
+    public BazaDanych() {
         try {
-
-            //Ustawiamy dane dotyczące podłączenia
-            conn = DriverManager.getConnection(polaczenieURL);
-
-            //Ustawiamy sterownik MySQL
-            Class.forName("com.mysql.jdbc.Driver");
-
-            //Uruchamiamy zapytanie do bazy danych
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(query);
-
-            while (rs.next()) {
-                wyswietlDaneZBazy(rs);
-            }
-
-            conn.close();
+            Class.forName(BazaDanych.sterownik);
+        } catch (ClassNotFoundException e) {
+            System.err.println("Brak sterownika");
+            e.printStackTrace();
         }
-        //Wyrzuć wyjątki jężeli nastąpią błędy z podłączeniem do bazy danych lub blędy zapytania o dane
-        catch (ClassNotFoundException wyjatek) {
-            System.out.println("Problem ze sterownikiem");
-        } catch (SQLException wyjatek) {
-            //e.printStackTrace();
-            //System.out.println("Problem z logowaniem\nProsze sprawdzic:\n nazwę użytkownika, hasło, nazwę bazy danych lub adres IP serwera");
-            System.out.println("SQLException: " + wyjatek.getMessage());
-            System.out.println("SQLState: " + wyjatek.getSQLState());
-            System.out.println("VendorError: " + wyjatek.getErrorCode());
-        }
-
-    }
-
-    static void wyswietlDaneZBazy(ResultSet rs) {
         try {
-            daneZBazy = rs.getString(1);
-            System.out.println("\n" + daneZBazy + " ");
-            daneZBazy = rs.getString(2);
-            System.out.println(daneZBazy + " ");
-            daneZBazy = rs.getString(3);
-            System.out.println(daneZBazy);
+            conn = DriverManager.getConnection(baza, "root", "arcon8211");
+            stat = conn.createStatement();
         } catch (SQLException e) {
+            System.err.println("Problem z otwarciem połaczenia");
             e.printStackTrace();
         }
     }
+
+
+    public void wyswietl() throws SQLException {
+
+
+        try {
+            ResultSet wynik = stat.executeQuery("select Id,Imie,Nazwisko " + "from dane.osoba" + " ORDER BY Id");
+            while (wynik.next()) {
+                System.out.println("ID:\t" + wynik.getString(1));
+                System.out.println("Imie:\t" + wynik.getString(2));
+                System.out.println("Nazwisko:\t" + wynik.getString(3));
+
+                System.out.println();
+            }
+
+        } finally {
+
+        }
+
+
+    }
+
 }
