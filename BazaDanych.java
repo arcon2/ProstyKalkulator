c674390590cb935ae6c89cb0f495ed2205eb6b4d
Arcon <arek@websitebest.pl>
2017-11-27 21:38:47 +0100

    01

--- BazaDanych.java
+++ BazaDanych.java
@@ -14,6 +14,7 @@ public class BazaDanych {
 
     //połaczenir z bazą
     public BazaDanych() {
+
         try {
             Class.forName(BazaDanych.sterownik);
         } catch (ClassNotFoundException e) {
